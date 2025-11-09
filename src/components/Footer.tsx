28f2ea26d976cce3f36581274a6f7874bffab110
halwave <halenw24@gmail.com>
2025-11-09 18:23:41 -0500

    Updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer: React.FC = () => (
-  <footer className="mt-12 text-pink-200 text-sm text-center py-4">
-    &copy; {new Date().getFullYear()} halwave. All rights reserved.
-  </footer>
-);
-
-export default Footer;
